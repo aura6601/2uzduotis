1a21f6e988685afe809469d2332a85834c41a5dc
aura6601 <55701863+aura6601@users.noreply.github.com>
2020-12-07 19:42:11 +0200

    Add files via upload

--- struktura.h
+++ struktura.h
@@ -3,14 +3,40 @@
 #include <iomanip>
 #include <string>
 #include <vector>
-#include <list>
 
-struct studentas{
-	string vardas;
-	string pavarde;
-	float galutinis;
-	vector<int> pazymiai;
-	int egzaminas;
+using namespace std; 
+
+class Studentas {
+
+private:
+	string vardas_;
+	string pavarde_;
+	int egzaminas_;
+	vector<int> pazymiai_;
+	double mediana_; 
+public:
+	Studentas() { };
+	Studentas(string v, string p, int e, vector<int>nd, double m);
+	~Studentas() {};
+	const string& getVardas_() {
+		return vardas_;
+	}
+	const string& getPavarde_() {
+		return pavarde_;
+	}
+	const int& getEgzaminas_() {
+		return egzaminas_;
+	}
+	const vector<int>& getPazymiai_() {
+		return pazymiai_;
+	}
+	const double& getMediana_() {
+		return mediana_;
+	}
+	inline string vardas() const { return vardas_; }   
+	inline string pavarde() const { return pavarde_; } 
+	double galBalas(int (*) (vector<int>) = mediana) const;  
+	istream& readStudent(std::istream&); 
 };
 
 struct vargsiukai {
