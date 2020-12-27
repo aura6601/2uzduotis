191806c14483a9fced22222b4c49c9bcb00f8010
aura6601 <55701863+aura6601@users.noreply.github.com>
2020-12-27 23:30:13 +0200

    Add files via upload

--- struktura.h
+++ struktura.h
@@ -1,63 +1,9 @@
 #pragma once
-#include <iostream>
-#include <iomanip>
-#include <string>
-#include <vector>
+#include "bibliotekos.h"
 
-using namespace std; 
-
-class Studentas {
-
-private:
-	string vardas_;
-	string pavarde_;
-	int egzaminas_;
-	vector<int> pazymiai_;
-	double mediana_; 
-public:
-	Studentas() { };
-	Studentas(string v, string p, int e, vector<int>nd, double m);
-	~Studentas() {};
-	const string& getVardas_() {
-		return vardas_;
-	}
-	const string& getPavarde_() {
-		return pavarde_;
-	}
-	const int& getEgzaminas_() {
-		return egzaminas_;
-	}
-	const vector<int>& getPazymiai_() {
-		return pazymiai_;
-	}
-	const double& getMediana_() {
-		return mediana_;
-	}
-	void setVardas_(string v) {
-		vardas_ = v;
-	}
-	void setPavarde_(string p) {
-		pavarde_ = p;
-	}
-	void setEgzaminas_(int e) {
-		egzaminas_ = e;
-	}
-	void setPazymiai_(vector<int> nd) {
-		pazymiai_ = nd;
-	}
-	void setMediana_(double m) {
-		mediana_ = m;
-	}
-};
-
-struct vargsiukai {
+struct studentas {
 	string vardas;
 	string pavarde;
-	float galutinis;
-};
-
-struct galvociai {
-	string vardas;
-	string pavarde;
-	float galutinis;
+	vector<int> pazymiai;
+	float egzaminas;
 };
