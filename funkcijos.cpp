819fcf7572c504cbb93afbedc35b0408398ad30f
aura6601 <55701863+aura6601@users.noreply.github.com>
2020-12-27 23:24:48 +0200

    Add files via upload

--- funkcijos.cpp
+++ funkcijos.cpp
@@ -1,81 +1,20 @@
 #include "funkcijos.h"
+#include "studentas.h"
 
-bool compareVardai(studentas a, studentas b) {
-    return a.vardas < b.vardas;
+bool compareVardai(Studentas a, Studentas b) {
+    return a.getVardas() < b.getVardas();
 }
-bool comparePavardes(studentas a, studentas  b) {
-    return a.pavarde < b.pavarde;
+bool comparePavardes(Studentas a, Studentas  b) {
+    return a.getPavarde() < b.getPavarde();
 }
 
-vector<studentas> nuskaitymas_vector() {
-	fstream failas;
-
-	string n;
-
-	do {
-		try {
-			cout << "Iveskite failo pavadinima: " << endl;
-			cin >> n;
-			failas.open(n);
-
-			if (failas.fail())
-			{
-				throw runtime_error("Failas nerastas");
-			}
-		}
-		catch (runtime_error& e) {
-			cout << "Klaida! Failas tokiu pavadinimu nerastas." << endl;
-			cout << "Jusu ivestis: " << n << endl;
-			cin.clear();
-			cin.ignore(10000, '\n');
-		}
-	} while (failas.fail());
-
-	int rows = 0, cols = 0;
-	string eilute, reiksme;
-	string vardas1, pavarde1, vardas, pavarde;
-	string rez;
-	vector<string> rezultatai1;
-	vector<string> rezultatai;
-	vector<studentas> grupe;
-	float galutinis;
-
-	while (getline(failas, eilute)) {
-		rows++;
-		if (rows == 1) {
-			stringstream ss(eilute);
-			while (ss >> reiksme) {
-				cols++;
-			}
-		}
-	}
-	failas.close();
-
-	failas.open(n);
-
-	if (failas.is_open()) {
-		for (int i = 0; i < 1; i++) {
-			failas >> vardas1 >> pavarde1;
-
-			for (int j = 0; j < cols - 2; j++) {
-				failas >> rez;
-				rezultatai1.push_back(rez);
-			}
-			rezultatai1.clear();
-			vector<string>().swap(rezultatai1);
-		}
-
-		for (int i = 1; i < rows; i++) {
-			failas >> vardas >> pavarde >> galutinis;
-
-			grupe.push_back(studentas{ vardas, pavarde, galutinis });
-
-		}
-	}
-	failas.close();
-	return grupe;
+Studentas::Studentas(string v, string p, vector<int> paz, int e, float g) : Zmogus(v, p) {
+	pazymiai = paz;
+	egzaminas = e;
+	galutinis = g;
 }
-list<studentas>nuskaitymas_list() {
+
+list<Studentas>nuskaitymas_list() {
     fstream failas;
 
 	string n;
@@ -105,7 +44,7 @@ list<studentas>nuskaitymas_list() {
 	string rez;
 	vector<string> rezultatai1;
 	vector<string> rezultatai;
-	list<studentas> grupe;
+	list<Studentas> grupe;
 	float galutinis;
 
 	while (getline(failas, eilute)) {
@@ -136,7 +75,13 @@ list<studentas>nuskaitymas_list() {
 		for (int i = 1; i < rows; i++) {
 			failas >> vardas >> pavarde >> galutinis;
 
-			grupe.push_back(studentas{ vardas, pavarde, galutinis });
+			Studentas studentas;
+
+			studentas.setVardas(vardas);
+			studentas.setPavarde(pavarde);
+			studentas.setGalutinis(galutinis);
+
+			grupe.push_back(studentas);
 
 		}
 	}
@@ -144,7 +89,9 @@ list<studentas>nuskaitymas_list() {
 	return grupe;
 }
 
-bool tikrinimas(const studentas& eilute) {
-	return eilute.galutinis < 5;
+bool tikrinimas(Studentas& eilute) {
+	return eilute.getGalutinis() < 5;
 }
+
+
 
