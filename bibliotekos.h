ff5106486c4feaf270d257f710e73cedb16984af
aura6601 <55701863+aura6601@users.noreply.github.com>
2020-12-27 23:02:29 +0200

    Add files via upload

---
+++ bibliotekos.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include <cctype>
+#include <iostream>
+#include <cstring>
+#include <fstream>
+#include <cstdio>
+#include <sstream> 
+#include <chrono>
+#include <list>
